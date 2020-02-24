c5a21f274bb50ae0ed23ac2c8611a36f54b24f9b
Tony Blard <tonyblard55@gmail.com>
2020-02-24 15:32:06 +0100

    button to update data in database in page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Api\MoodleApi;
+use App\Controller\MoodleApiController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -10,8 +13,15 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request, MoodleApiController $moodleApiController, MoodleApi $moodleApi)
     {
+        if ($request->isMethod('POST')) {
+            $moodleApiController->postQuiz($moodleApi);
+            $moodleApiController->postCandidate($moodleApi);
+            $moodleApiController->postCriteria($moodleApi);
+            $moodleApiController->postResult($moodleApi);
+            $moodleApiController->postcoef($moodleApi);
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
