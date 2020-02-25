b9077e2dabf8b5f0a4e153aee1892ce47bd1b349
Dylan <dylngae@outlook.com>
2020-02-25 10:21:10 +0100

    add admin profil

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -4,16 +4,19 @@ namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin_profil")
      */
-    public function index()
+    public function teachersList()
     {
+        $teachers = $this->getDoctrine()->getRepository(User::class)->findAll();
+        
         return $this->render('admin/profil.html.twig', [
-            'controller_name' => 'AdminController',
+            'teachers' => $teachers,
         ]);
     }
 }
