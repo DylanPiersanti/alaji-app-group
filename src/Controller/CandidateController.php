833cb73ef5426e4304487fe6cee95867ea05269b
Dylan <dylngae@outlook.com>
2020-02-25 13:36:14 +0100

    add marks and averages

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -4,8 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 use App\Entity\Candidate;
 use App\Entity\Result;
+use App\Api\MoodleApi;
+
 
 
 class CandidateController extends AbstractController
@@ -29,14 +33,83 @@ class CandidateController extends AbstractController
     /**
      * @Route("/quiz/candidates/{id}", name="candidate_profil")
      */
-    public function index(int $id)
+    public function index(int $id, MoodleApi $moodleApi, Request $request)
     {
 
         $candidate = $this->getDoctrine()->getRepository(Candidate::class)->find($id);
 
+        if ($request->isMethod('POST')) {
+            $submittedToken = $request->request->get('token');
+            if ($this->isCsrfTokenValid('addCriteria', $submittedToken)) {
+                $oral1 = intval($_POST['criteria1']);
+                $oral2 = intval($_POST['criteria2']);
+                $oral3 = intval($_POST['criteria3']);
+                $oral4 = intval($_POST['criteria4']);
+
+                $results = $this->getDoctrine()->getRepository(Result::class)->findOneBy(['candidate' => $candidate, 'criteria' => 1]);
+                $results->setOral($oral1);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($results);
+
+
+                $results = $this->getDoctrine()->getRepository(Result::class)->findOneBy(['candidate' => $candidate, 'criteria' => 2]);
+                $results->setOral($oral2);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($results);
+
+
+                $results = $this->getDoctrine()->getRepository(Result::class)->findOneBy(['candidate' => $candidate, 'criteria' => 3]);
+                $results->setOral($oral3);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($results);
+
+
+                $results = $this->getDoctrine()->getRepository(Result::class)->findOneBy(['candidate' => $candidate, 'criteria' => 4]);
+                $results->setOral($oral4);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($results);
+
+                $manager->flush();
+
+            }
+            $results = $this->getDoctrine()->getRepository(Result::class)->findBy(['candidate' => $candidate]);
+            foreach ($results as $result) {
+
+                $oral = $result->getOral();
+                $test = $result->getTest();
+                $coeftest = $result->getCoeftest();
+                $coefOral = $result->getCoeforal();
+                $average  = $moodleApi->averageResult($test, $coeftest, $oral, $coefOral);
+
+                $acquis = $moodleApi->acquis($average);
+
+
+                $result->setAverage($average);
+                $result->setAcquis($acquis);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($result);
+                $manager->flush();
+
+            }
+        }
+
         $results = $this->getDoctrine()->getRepository(Result::class)->findBy(['candidate' => $candidate]);
 
         return $this->render('candidate/index.html.twig', [
+            'candidate' => $candidate,
+            'results' => $results
+        ]);
+    }
+
+    /**
+     * @Route("quiz/candidates/{id}/addoral", name="student_form")
+     */
+    public function getFormCandidate(int $id, Request $request)
+    {
+        $candidate = $this->getDoctrine()->getRepository(Candidate::class)->find($id);
+        $results = $this->getDoctrine()->getRepository(Result::class)->findBy(['candidate' => $candidate]);
+
+        return $this->render('candidate/addoral.html.twig', [
             'candidate' => $candidate,
             'results' => $results
         ]);
